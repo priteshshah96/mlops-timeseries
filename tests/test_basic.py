b3d6e5bca6fff4135701f9919b34296c64c40942
priteshshah96 <priteshshahwork@gmail.com>
2024-12-06 14:32:07 -0500

    Add basic test structure and configuration

---
+++ tests/test_basic.py
@@ -0,0 +1,10 @@
+﻿from mlops_timeseries import __version__
+
+def test_version():
+    '''Test version is not None'''
+    assert __version__ is not None
+
+def test_import():
+    '''Test basic imports'''
+    import mlops_timeseries
+    assert True
