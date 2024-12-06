a0240871551c884b16cd1aa22b83193523002a0a
priteshshah96 <priteshshahwork@gmail.com>
2024-12-06 14:33:43 -0500

    Fix linting error in basic tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,11 @@
-﻿from mlops_timeseries import __version__
+﻿"""Basic tests for mlops_timeseries package."""
+from mlops_timeseries import __version__
 
 def test_version():
-    '''Test version is not None'''
+    """Test version is not None."""
     assert __version__ is not None
 
 def test_import():
-    '''Test basic imports'''
+    """Test basic imports work correctly."""
     import mlops_timeseries
-    assert True
+    assert hasattr(mlops_timeseries, '__version__')
